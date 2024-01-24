2bf431e8b9732f63c18f59742a04ab630a297a84
Ingrid Novak <ingridnovak240@gmail.com>
2024-01-24 20:53:08 +0200

    added 3 buttons, refactored code

--- script/script.js
+++ script/script.js
@@ -4,39 +4,58 @@
   
 let yourCount = 0;
 let compCount = 0;
-let result = 0;
+
 let button = document.getElementById('play');
 
-button.addEventListener('click', () => { console.log(game()); })
+let res = document.getElementById('result');
+let count = document.getElementById('count');
 
+let playerSelection;
+let choses = document.querySelector('#choose');
+choses.addEventListener('click',(e) => {
+    let target = e.target;
+    switch(target.id){
+        case "rock" :
+        playerSelection = "rock";
+        break;
+        case "paper":
+        playerSelection = "paper";
+        break;
+        case "scissors":
+        playerSelection = "scissors";
+        break;
+
+    }
+   game()
+})
 
 
 function stopGame(){
    
-      
-        button.innerHTML = 'play again!';
-        result = 0;
-        compCount = 0;
-        yourCount = 0;
-
-    if(yourCount > compCount){
-        return 'You win!'
+    console.log(compCount + '- compcount' + yourCount + '- yourcount')
+    if(yourCount === compCount){
+        res.textContent =  "Finish! it's a tie."
     }else if( compCount > yourCount){
-        return 'You lose('
+        res.textContent =  'Finish! You lose('
     }else {
-        return "it's a tie."
+        res.textContent =  'Finish! You win!'
     }
+
+    compCount = 0;
+    yourCount = 0;
+
 
     
 }
 
 function game(){
-    result += 1;
-    if(result === 5){
+    count.textContent = "Your count : " + yourCount + ' Computer count : ' + compCount;
+   if(compCount === 5 || yourCount === 5){
+   
         return (stopGame());
-    }
     
-    const playerSelection = prompt('Enter your move(scissors, rock or paper?): ').toLowerCase();
+}
+   
 function getComputerChoice(){
     let arr = ['rock', 'scissors', 'paper'];
     let index = Math.floor(Math.random() * arr.length);
@@ -45,6 +64,9 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection) {
     console.log('player - ' + playerSelection + ' and computer ' + computerSelection)
+
+  
+
 
     if(playerSelection === computerSelection){
         return 0;
@@ -59,15 +81,17 @@ function playRound(playerSelection, computerSelection) {
 }   
     switch(playRound(playerSelection, getComputerChoice())){
         case 0:
-            return "It's a tie";
-            break;
+           res.textContent = "It's a tie";
+          
+           
+           break;
         case 1:
             yourCount +=1;
-            return 'You win! You count is ' + yourCount  + ' and computer is ' + compCount;
+            res.textContent =  'You win! You count is ' + yourCount  + ' and computer is ' + compCount;
             break;
         case -1:
             compCount +=1;
-            return 'You Lose! You count is ' + yourCount + ' and computer is ' + compCount;
+            res.textContent =  'You Lose! You count is ' + yourCount + ' and computer is ' + compCount;
             break;
     }
 
